b4676f070db4688fa1f8e542d9508cb2c4579bc3
Valentin Hilbig <webmaster@scylla-charybdis.com>
2004-04-08 21:38:36 +0000

    Further improvements for SQL writing.  Some BUGs removed, too.

--- old/buf.h
+++ old/buf.h
@@ -1,7 +1,10 @@
 /* $Header$
  *
  * $Log$
- * Revision 1.1  2004-04-07 02:22:48  tino
+ * Revision 1.2  2004-04-08 21:38:36  tino
+ * Further improvements for SQL writing.  Some BUGs removed, too.
+ *
+ * Revision 1.1  2004/04/07 02:22:48  tino
  * Prototype for storing data in gff_lib done (untested)
  *
  */
@@ -10,6 +13,7 @@
 #define tino_INC_buf_h
 
 #include "alloc.h"
+#include "codec.h"
 
 typedef struct tino_buf
   {
@@ -45,12 +49,18 @@ tino_buf_add_c(TINO_BUF *buf, char c)
   TINO_BUF_ADD_C(buf, c);
 }
 
-static void
-tino_buf_add_n(TINO_BUF *buf, const void *ptr, size_t len)
+static __inline__ char *
+tino_buf_add_ptr(TINO_BUF *buf, size_t len)
 {
   if (buf->fill+len>=buf->max)
     tino_buf_extend(buf, buf->fill+len-buf->max+1);
-  memcpy(buf->data+buf->fill, ptr, len);
+  return buf->data+buf->fill;
+}
+
+static void
+tino_buf_add_n(TINO_BUF *buf, const void *ptr, size_t len)
+{
+  memcpy(tino_buf_add_ptr(buf, len), ptr, len);
   buf->fill	+= len;
 }
 
@@ -73,6 +83,22 @@ tino_buf_get_s(TINO_BUF *buf)
   return buf->data;
 }
 
+static const char *
+tino_buf_get(TINO_BUF *buf)
+{
+  if (!buf)
+    return 0;
+  return buf->data;
+}
+
+static size_t
+tino_buf_len(TINO_BUF *buf)
+{
+  if (!buf)
+    return 0;
+  return buf->fill;
+}
+
 static void
 tino_buf_free(TINO_BUF *buf)
 {
@@ -89,6 +115,22 @@ tino_buf_swap(TINO_BUF *a, TINO_BUF *b)
   x	= *a;
   *a	= *b;
   *b	= x;
+}
+
+static int
+tino_buf_add_hex(TINO_BUF *buf, const char *s)
+{
+  size_t	len;
+  char		*tmp;
+  int		i;
+
+  if (!buf || !s)
+    return -1;
+  len	= strlen(s);
+  tmp	= tino_buf_add_ptr(buf, len);
+  i	= tino_dec_hex(tmp, len, s);
+  buf->fill	+= i;
+  return s[i+i] ? 1 : 0;
 }
 
 #endif
