e11c7dd0a31f01902f8e1eee86b44fbd11634237
Administrator <Administrator>
2004-08-22 05:47:33 +0000

    Now Makefile generator understands to look into variables set in Makefile. This way Auto-Dependicies can be improved a little step and more important, the "TINOCOPY" functionality was added, such that I can copy files from elsewhere to the distribution and keep that files in sync.

---
+++ old/Makefile-tino.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+# $Header$
+#
+# Maintain Makefile.md5 file, a list of MD5 sums of files which are
+# automatically generated (like the Makefile).
+#
+# $Log$
+# Revision 1.1  2004-08-22 05:49:49  Administrator
+# Now Makefile generator understands to look into variables set in Makefile.
+# This way Auto-Dependicies can be improved a little step and more important,
+# the "TINOCOPY" functionality was added, such that I can copy files from
+# elsewhere to the distribution and keep that files in sync.
+#
+
+#set -x
+
+# Calculate MD5 sum
+: md5
+md5()
+{
+  eval "$1"='"$(md5sum "$2" 2>/dev/null)"'
+  eval "$1=\"\${$1%% *}\""
+}
+
+# Fetch MD5 sum from MD5 list
+# Easy (stupid) implementation
+: md5get
+md5get()
+{
+  while read $1 name
+  do
+	[ ".$2" = ".$name" ] && return
+  done < Makefile.md5
+  eval $1=
+  return 1
+}
+
+# Compare if file matches MD5 sum
+: md5cmp
+md5cmp()
+{
+  if [ -n "$was" ]
+  then
+	echo "The integrity of $2 is unknown."
+
+  elif	md5 cmp2 "$2" || exit
+	[ ".$cmp2" = ".$1" ]
+  then
+	return 0
+  else
+	echo "The file $2 was edited."
+  fi
+  echo "The new version is at $3"
+  return 1
+}
+
+# Insert/Replace MD5 sum in MD5 list
+: md5set
+md5set()
+{
+  awk -vMD5="$1" -vNAME="$2" '
+BEGIN	{
+	while ((getline < "Makefile.md5")>0)
+		{
+		md5=$1
+		sub(/[^ ]* /,"");
+		has[$0]=md5
+		}
+	close("Makefile.md5")
+
+	has[NAME] = MD5
+	for (a in has)
+		print has[a] " " a > "Makefile.md5"
+	}' </dev/null || exit
+}
+
+# This version has too much overhead
+# in case a lot of is to do
+: md5copy
+md5copy_old()
+{
+  md5 new "$1" || exit  
+  md5get was "$2"
+
+  ret=:
+  if cmp -s "$1" "$2"
+  then
+	echo "$2 is up to date."
+  elif [ ! -f "$2" ] || md5cmp "$was" "$2" "$1"
+  then
+	echo "$2 updated."
+	rm -f "$2"
+	cp -f "$1" "$2" || exit
+  else
+	ret=false
+  fi
+#  echo "was=$was new=$new"
+  [ ".$was" != ".$new" ] && md5set "$new" "$2"
+  $ret
+}
+
+: md5copy
+md5copy()
+{
+  md5 new "$1" || exit
+  if [ ! -f "$2" ]
+  then
+	echo "$2 created."
+	rm -f "$2"	# may be a dangling softlink
+	cp -f "$1" "$2"
+  fi
+  md5 old "$2"
+  awk -vOLD="$old" -vNEW="$new" -vNAME="$2" -vFROM="$1" '
+BEGIN	{
+	while ((getline < "Makefile.md5")>0)
+		{
+		md5=$1;
+		sub(/[^ ]* /,"");
+		has[$0]=md5;
+		}
+	close("Makefile.md5");
+
+	if (OLD==NEW)
+		ret=0;
+	else if (has[NAME]==OLD)
+		ret=1;
+	else if (has[NAME]=="")
+		{
+		print "The integrity of " NAME " is unknown.";
+		# Leave it in this state!
+		exit(2);
+		}
+	else
+		{
+		print "The file " NAME " was edited."
+		ret=2;
+		}
+
+	if (has[NAME]!=NEW)
+		{
+		has[NAME]=NEW
+		for (a in has)
+			print has[a] " " a > "Makefile.md5";
+		}
+	exit(ret);
+	}' </dev/null
+  case "$?" in
+  0)	;;
+  1)	echo "$2 updated."
+	rm -f "$2"
+	cp -f "$1" "$2" || exit
+	;;
+  2)	echo "The new version is at $1"
+	echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+	false
+	;;
+  esac
+}
+
+
+here="`pwd`"
+cd "$1" || exit
+shift
+
+awk -vSRC="$here" -f"$here/Makefile.awk" Makefile.tino "$here/Makefile.proto" >Makefile.~ || exit
+
+md5copy Makefile.~ Makefile && rm -f Makefile.~
+
+for tf
+do
+	md5copy "$tf" "${tf//\//_}"
+done
--- old/Makefile.awk
+++ old/Makefile.awk
@@ -8,7 +8,13 @@
 # as it does not support all the options I want it to support!
 #
 # $Log$
-# Revision 1.2  2004-07-28 03:42:19  tino
+# Revision 1.3  2004-08-22 05:47:34  Administrator
+# Now Makefile generator understands to look into variables set in Makefile.
+# This way Auto-Dependicies can be improved a little step and more important,
+# the "TINOCOPY" functionality was added, such that I can copy files from
+# elsewhere to the distribution and keep that files in sync.
+#
+# Revision 1.2  2004/07/28 03:42:19  tino
 # -
 #
 # Revision 1.1  2004/07/21 13:29:14  tino
@@ -36,6 +42,30 @@ FILENAME!=lastfile	{
 	print "# included: " FILENAME
 	print "#"
 	empty=1
+	continuation=0
+	}
+
+# Set the variables to loop over
+/^#S/	{
+	delete splitted
+	for (i=2; i<=NF; i++)
+	  splitter($i)
+	gather=""
+	}
+
+# Gather all the rulesets 
+/^#R/	{
+	if (/^#R /)
+		gather=gather substr($0,4) "\n"
+	else
+		gather=gather substr($0,3) "\n"
+	next
+	}
+
+# Expand rules with #v# replaced by the variable part..
+# The lines are repeated for all the variable content's parts.
+gather!=""	{
+	flusher()
 	}
 
 # copy CVS tags as comments
@@ -47,20 +77,30 @@ FILENAME!=lastfile	{
 	next;
 	}
 
+# Print out selected comments
+/^#C/	{
+	print substr($0,3)
+	if ($0=="#C")
+	  {
+	    empty=0
+	    continuation=0
+	    next
+	  }
+	}
+
 # Magic, stop this file and go to the next
 /^##END##/	{
-	empty=1
 	nextfile;
 	}
 
 # Fold comments to one empty line
-/^#/	{
-	empty=1;
-	next;
-	}
-
 # compress empty lines to one
-/^[[:space:]]*$/	{ empty=1; next; }
+
+/^#/ || /^[[:space:]]*$/	{
+	empty=1
+	continuation=0
+	next
+	}
 
 # Print all other lines unchanged
 	{
@@ -69,13 +109,49 @@ FILENAME!=lastfile	{
 	print
 	}
 
+/^[[:space:]]*[A-Z]*=/	{
+	gsub(/^[[:space:]]*/,"")
+        n=index($0,"=")
+        name=substr($0,1,n-1)
+	sub(/^[^=]*=/,"")
+        var[name]=""
+        continuation=1
+	}
+continuation {
+	gsub(/^[[:space:]]*/,"")
+        continuation = /\\$/
+	gsub(/[[:space:]]*\\?$/,"")
+        var[name]=var[name] " " $0
+	}
+
 # Write the basic dependencies and end marker
 END	{
-	print ""
-	print "#"
-	print "# end of includes"
-	print "#"
-	print "# Automatically created dependencies:"
-	print ""
-	print "# (not yet implemented)"
+	flusher();
+	print "# end"
 	}
+
+function splitter(v,a,k)
+{
+#  print "#" v "=" var[v]
+  split(var[v],a,/[[:space:]]*/);
+  for (k in a)
+    if (a[k] ~ /^[-./a-zA-Z0-9]+$/)
+      splitted[a[k]]=1
+}
+
+function flusher(v,s,p)
+{
+  for (v in splitted)
+    {
+      s=gather
+  
+      p=v
+      sub(/\//,"_",p)
+  
+      gsub(/#v#/,v,s)
+      gsub(/#p#/,p,s)
+      printf "%s", s
+      empty=1
+    }
+  gather=""
+}
