9350cbe40bfc03277da2d096b219a6efd6ee33d0
Valentin Hilbig <webmaster@scylla-charybdis.com>
2004-10-05 02:05:11 +0000

    added (prototypes)

---
+++ old/io.h
@@ -0,0 +1,23 @@
+/* $Header$
+ *
+ * Generic IO layer.  For the moment this only includes tino/file.h
+ * IF YOU WANT TO DEPEND ON THS, USE IT AS THE FIRST INCLUDE!
+ *
+ * In some distant future this will handle *all* IO the same way,
+ * regardless if it is a file, directory, device, socket or telepathy.
+ * YKWIM.
+ *
+ * Please read io.txt for more information.
+ *
+ * $Log$
+ * Revision 1.1  2004-10-05 02:05:11  tino
+ * added (prototypes)
+ *
+ */
+
+#ifndef tino_INC_io_h
+#define tino_INC_io_h
+
+#include "file.h"
+
+#endif
