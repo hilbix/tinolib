6b3291dab6cf868f87b9eabca3716049998ad27d
Valentin Hilbig <webmaster@scylla-charybdis.com>
2008-05-29 21:42:53 +0000

    added

---
+++ old/shit.h
@@ -0,0 +1,239 @@
+/* $Header$
+ *
+ * THIS IS NOT READY YET *
+ * UNIT TEST FAILS *
+ *
+ * Shell Helper Integrated Transfer
+ *
+ * Copyright (C)2008 by Valentin Hilbig <webmaster@scylla-charybdis.com>
+ *
+ * This is release early code.  Use at own risk.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ *
+ * $Log$
+ * Revision 1.1  2008-05-29 21:42:53  tino
+ * added
+ *
+ */
+
+#ifndef tino_INC_shit_h
+#define tino_INC_shit_h
+
+#include "alloc.h"
+
+#if 0
+/* This is an integration method to fork off some shell helpers as
+ * co-processes in a standard way.  The helpers need only be forked
+ * once or even can run on remote systems via sockets to perform their
+ * tasks, as all commandline parameters etc. are transferred via
+ * stdin/out.
+ *
+ * SHIT mode is run with the --shit option, which can be invoked via
+ * tino_getopt() with following part:
+ *
+ *	TINO_GETOPT_IGNORE TINO_GETOPT_LLOPT TINO_GETOPT_FN
+ *	"shit	Shell Helper Integrated Transfer (do not use)"
+ *	, shit_mode,
+ *
+ * This calls a code snippet as follows (note: the args are not used):
+ */
+const char *
+shit_mode(void *ptr, const char *arg, const char *opt, void *usr)
+{
+  struct tino_shit	shit;
+  struct tino_shit_io	*me, *r;
+  const char		*id;
+
+  if ((id=getenv("TINO_SHIT_MODE"))==0)
+    return "SHIT mode cannot be used manually";
+
+  tino_shit_initO(&shit, "HELPER_NAME");
+  me	= tino_shit_helperO(&shit, 0, 1, id);
+
+  while ((r=tino_shit_request_inN(me))!=0)
+    {
+      tino_shit_answer_spoolO(r);
+      process_string(tino_shit_stringO(r));
+      tino_shit_answer_finishO(r);
+
+      tino_shit_closeO(r);
+    }
+
+  tino_shit_exitO(&shit, 0);	// Free resources, exit(0)
+}
+
+/* The server side is likewise easy to use:
+ */
+struct tino_shit	shit;
+struct tino_shit_io	*helper;
+
+tino_shit_initO(&shit, "INTEGRATOR_NAME");
+helper	= tino_shit_fork_argsN(&shit, "/path/to/program", "--shit", NULL);
+
+/* Send a request to the helper and receive the response
+ */
+const char *
+send_request(const char *req)
+{
+  struct tino_shit_io	*r;
+
+  r	= tino_shit_request_outO(helper);
+  tino_shit_write_stringO(r, req);
+  tino_shit_sendO(r);
+  answer	= strdup(tino_shit_stringO(r));
+  tino_shit_closeO(r);
+
+  return answer;
+}
+
+tino_shit_closeO(helper);	// Close the helper
+tino_shit_endO(&shit);		// Free resources
+
+/* You can fork off more than one helper in parallel, you can have
+ * more than one request active in parallel, and you can send and
+ * receive more than one argument and variably terminated argument
+ * lists.
+ *
+ * Any communication error (like squence errors) immediately
+ * terminates the helper and the connection to it.  If automatic error
+ * handling is not overwritten, the integrator (the one calling the
+ * helpers) terminates, too, to be on the safe side.
+ */
+#endif
+
+
+struct tino_shit
+  {
+    const char		*name;
+    struct tino_shit_io	*io;
+  };
+
+struct tino_shit_io
+  {
+    struct tino_shit_io	*next;
+    struct tino_shit	*shit;
+  };
+
+/* Close the handle, free resources and return exit termination state
+ * (exit status) of the connection
+ */
+static int
+tino_shit_closeO(struct tino_shit_io *io)
+{
+  000;
+  return -1;
+}
+
+/* Terminate the SHIT structure
+ */
+static void
+tino_shit_endO(struct tino_shit *shit)
+{
+  while (shit->io)
+    tino_shit_closeO(shit->io);
+  tino_free_constO(shit->name);
+  shit->name	= 0;
+  000;
+}
+
+static void
+tino_shit_exitO(struct tino_shit *shit, int exit_val)
+{
+  000;	/* XXX send exit code to the other side	*/
+  tino_shit_endO(shit);
+  exit(exit_val);
+}
+
+static void
+tino_shit_initO(struct tino_shit *shit, const char *name)
+{
+  shit->io	= 0;
+  shit->name	= tino_strdupO(name);
+  000;
+}
+
+/* Start a helper.
+ *
+ * I is the input FD (usually 0)
+ * O is the output FD (usually 1)
+ * ID is what was transferred via getopt("TINO_SHIT_MODE")
+ */
+static struct tino_shit_io *
+tino_shit_helperO(struct tino_shit *shit, int i, int o, const char *id)
+{
+  000;
+  return 0;
+}
+
+/* Receive a request.
+ *
+ * Usually done on the helper side.
+ */
+static struct tino_shit_io *
+tino_shit_request_inN(struct tino_shit_io *helper)
+{
+  000;
+  return 0;
+}
+
+/* Start spooling the answer.
+ *
+ * This means, the helper starts sending arbitrary data to stdout.
+ *
+ * This is implemented by telling some (hopefully) random string to
+ * the other side which does not show up in the data.
+ */
+static void
+tino_shit_answer_spoolO(struct tino_shit_io *io)
+{
+  000;
+}
+
+/* Stop spooling the answer.
+ *
+ * The data is terminated by the above string.
+ */
+static void
+tino_shit_answer_finishO(struct tino_shit_io *io)
+{
+  000;
+}
+
+/* This returns the next string from the IO.
+ *
+ * Note that this string is kept in memory until the request is
+ * closed by tino_shit_closeO() or tino_shit_freeO()
+ */
+static const char *
+tino_shit_stringO(struct tino_shit_io *io)
+{
+  000;
+  return 0;
+}
+
+/* Free the string which was allocated by tino_shit_stringO()
+ *
+ * Often you do not need to do this, as this is implicitly done with
+ * tino_shit_closeO()
+ */
+static void
+tino_shit_freeO(struct tino_shit_io *io, const void *s)
+{
+  000;
+}
+
+#endif
