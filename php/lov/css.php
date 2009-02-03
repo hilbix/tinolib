e44a11f376ea20112ecb487f45ed92615a3e624d
Valentin Hilbig <webmaster@scylla-charybdis.com>
2009-02-03 17:13:05 +0000

    Current

--- php/lov/css.php
+++ php/lov/css.php
@@ -2,18 +2,25 @@
 # $Header$
 #
 # $Log$
+# Revision 1.2  2009-02-03 17:13:05  tino
+# Current
+#
 # Revision 1.1  2008-06-22 11:32:20  tino
 # First checkin
-#
 
 header("Content-type: text/css");
 ?>
 TABLE	{ border:1px solid black; border-spacing:0px; padding:3px; empty-cells:hide }
 TR	{ border-top:1px; border-bottom:1px }
-TD	{ padding:0px; }
+TD	{ padding:1px; }
 TD.data	{ padding:1px; padding-left:1mm; padding-right:1mm; border-spacing:0px; white-space:nowrap }
 .line0	{ background-color:#eeeeee }
 .line1	{ background-color:#dddddd }
 .line0:hover	{ background-color:#8080ff }
 .line1:hover	{ background-color:#8080ff }
 tr.line	{ border:2px solid black; empty-cells:show; background-color:#000000 }
+
+.m400		{ max-width:400px; overflow:auto }
+.m500		{ max-width:500px; overflow:auto }
+.m600		{ max-width:600px; overflow:auto }
+
