298e48269913b6c1f405a00fbf0f3ae4ba2861b2
Valentin Hilbig <webmaster@scylla-charybdis.com>
2013-02-14 13:23:35 +0100

    bugfix

--- setuptino.sh
+++ setuptino.sh
@@ -2,21 +2,8 @@
 #
 # Convenience script to setup new directory
 #
-# Copyright (C)2005-2013 Valentin Hilbig, webmaster@scylla-charybdis.com
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# This Works is placed under the terms of the Copyright Less License,
+# see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
 
 TARG=tino
 DIET=diet
@@ -103,7 +90,7 @@ then
 			now=OOPS
 			break
 		fi
-		[ / = "$PWD" ] || fail "mountpoint does not work"
+		[ / = "$PWD" ] && fail "mountpoint does not work"
 		cd ..
 	done >/dev/null
 	case "$here" in
