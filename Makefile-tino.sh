854c7486cf80cd5b1f63eb560e44e4051d923e7f
Valentin Hilbig <webmaster@scylla-charybdis.com>
2018-09-11 13:33:58 +0200

    Makefile-tino.sh modified

--- Makefile-tino.sh
+++ Makefile-tino.sh
@@ -109,7 +109,7 @@ here="${here#`pwd`/}"
 make -f -
 
 "$GAWK" -vSRC="$here" -f"$here/Makefile.awk" Makefile.tino "$here/Makefile.proto" >Makefile.~ ||
-{ echo $'\n###\n'"### Your $GAWK is no gawk, please install gawk!"$'\n###\n'; exit 1; }
+{ echo $'\n###\n'"### Your $GAWK is no gawk, please install gawk!"$'\n### (using Makefile as-is instead)\n###\n'; exit 1; }
 
 md5copy Makefile.~ Makefile && rm -f Makefile.~
 
