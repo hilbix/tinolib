8a2351e826a41080fb8ee2915e00fa474488189f
Valentin Hilbig <webmaster@scylla-charybdis.com>
2018-12-17 17:02:42 +0100

    squash! assoc.h

--- assoc.h
+++ assoc.h
@@ -35,9 +35,10 @@ struct tino_assoc
 struct tino_assoc_iter
   {
     struct tino_assoc		*a;
-    const struct tino_assoc_el	*p;
-    const void			*k, *v;
-    int				r;
+    struct tino_assoc_el	*e, *p;
+    const void			*k;
+    void			*v;
+    int				r:1, c:1;
   };
 
 struct tino_assoc_el
@@ -112,6 +113,52 @@ static struct tino_assoc_ops tino_assoc_str2str =
 
 /**********************************************************************/
 
+TINO_INLINE(static void
+_tino_assoc_remove(TINO_ASSOC a, TINO_ASSOC_EL e))
+{
+  TINO_FATAL_IF(!e);
+
+  if (e->next)
+    e->next->prev	= e->prev;
+  if (e->prev)
+    e->prev->next	= e->next;
+  if (a->first == e)
+    a->first	= e->next;
+  if (a->last == e)
+    a->last	= e->prev;
+  e->next	= 0;
+  e->prev	= 0;
+}
+
+/* If an element is given, prepend to the given element
+ * (insert it at the given position).
+ *
+ * NULL means, append at the end, a->first means prepend to assoc.
+ */
+TINO_INLINE(static void
+_tino_assoc_insert(TINO_ASSOC a, TINO_ASSOC_EL e, TINO_ASSOC_EL p))
+{
+  _tino_assoc_remove(a, e);
+
+  if (a->first == p)
+    a->first	= e;
+  if (p)
+    {
+      e->next	= p;
+      e->prev	= p->prev;
+      if (e->prev)
+        e->prev->next	= e;
+      p->prev	= e;
+    }
+  else
+    {
+      e->prev	= a->last;
+      if (a->last)
+        a->last->next	= e;
+      a->last	= e;
+    }
+}
+
 static TINO_ASSOC_EL
 _tino_assoc_new(TINO_ASSOC a, const void *key)
 {
@@ -119,21 +166,41 @@ _tino_assoc_new(TINO_ASSOC a, const void *key)
 
   e		= tino_allocO(sizeof *e);
   e->next	= 0;
-  e->prev	= a->last;
+  e->prev	= 0;
   e->k		= a->ops->knew(a->user, key);
-  e->v		= a->ops->knew(a->user, key);
+  e->v		= 0;
   e->_const	= 0;
 
+  _tino_assoc_insert(a, e, NULL);
   a->count++;
-
-  if (a->last)
-    a->last->next	= e;
-  a->last	= e;
-  if (!a->first)
-    a->first	= e;
-
   return e;
 }
+
+static void
+_tino_assoc_vset(TINO_ASSOC a, TINO_ASSOC_EL e, void *v, int _const)
+{
+  if (e->v)
+    a->ops->vfree(a->user, e->v);
+  e->v		= a->ops->vnew(a->user, v);
+  e->_const	= !!_const;
+}
+
+static void
+_tino_assoc_free(TINO_ASSOC a, TINO_ASSOC_EL e)
+{
+  _tino_assoc_remove(a, e);
+  a->count--;
+
+  a->ops->vfree(a->user, e->v);
+  e->v		= 0;
+
+  a->ops->kfree(a->user, (void *)e->k);
+  e->k		= 0;
+
+  tino_freeO(e);
+}
+
+/**********************************************************************/
 
 /* lookup is O(n) for now
  *
@@ -149,30 +216,6 @@ _tino_assoc_bykey(TINO_ASSOC a, const void *key, int create)
     if (!a->ops->kcmp(a->user, key, e->k))
       return e;
   return create ? _tino_assoc_new(a, key) : 0;
-}
-
-static void
-_tino_assoc_vset(TINO_ASSOC a, TINO_ASSOC_EL e, void *v, int _const)
-{
-  if (e->v)
-    a->ops->vfree(a->user, e->v);
-  e->v		= a->ops->vnew(a->user, v);
-  e->_const	= _const;
-}
-
-static void
-_tino_assoc_remove(TINO_ASSOC a, TINO_ASSOC_EL e)
-{
-  TINO_FATAL_IF(!e);
-
-  if (e->next)
-    e->next->prev	= e->prev;
-  if (e->prev)
-    e->prev->next	= e->next;
-  if (a->first == e)
-    a->first	= e->next;
-  if (a->last == e)
-    a->last	= e->prev;
 }
 
 /**********************************************************************/
@@ -206,27 +249,33 @@ tino_assoc_free(TINO_ASSOC a)
 {
   cDP(("(%p)", a));
   while (a->first)
-    _tino_assoc_remove(a, a->first);
+    _tino_assoc_free(a, a->first);
   TINO_FATAL_IF(a->count);
   if (a->_alloc)
     tino_freeO(a);
 }
 
+/* Set key to const value
+ */
 static void
-tino_assoc_set(TINO_ASSOC a, const void *k, void *v)
+tino_assoc_set(TINO_ASSOC a, const void *k, const void *v)
 {
   cDP(("(%p,%p,%p)", a, k, v));
-  return _tino_assoc_vset(a, _tino_assoc_bykey(a, k, 1), v, 0);
+  _tino_assoc_vset(a, _tino_assoc_bykey(a, k, 1), (void *)v, 1);
 }
 
+/* Set key to nonconst value
+ */
 static void
-tino_assoc_setc(TINO_ASSOC a, const void *k, const void *v)
+tino_assoc_nset(TINO_ASSOC a, const void *k, void *v)
 {
   cDP(("(%p,%p,%p)", a, k, v));
-  return _tino_assoc_vset(a, _tino_assoc_bykey(a, k, 1), (void *)v, 1);
+  _tino_assoc_vset(a, _tino_assoc_bykey(a, k, 1), v, 0);
 }
 
-static void *
+/* Get pointer to const value
+ */
+static const void *
 tino_assoc_get(TINO_ASSOC a, const void *k)
 {
   TINO_ASSOC_EL	e;
@@ -234,18 +283,20 @@ tino_assoc_get(TINO_ASSOC a, const void *k)
   cDP(("(%p,%p)", a, k));
   e	= _tino_assoc_bykey(a, k, 0);
   cDP(("() %p %p", e, e ? e->v : NULL));
-  TINO_FATAL(("nonconstant access to constant assoc element", e->_const));
   return e ? e->v : NULL;
 }
 
-static const void *
-tino_assoc_getc(TINO_ASSOC a, const void *k)
+/* Get pointer to nonconst value
+ */
+static void *
+tino_assoc_nget(TINO_ASSOC a, const void *k)
 {
   TINO_ASSOC_EL	e;
 
   cDP(("(%p,%p)", a, k));
   e	= _tino_assoc_bykey(a, k, 0);
   cDP(("() %p %p", e, e ? e->v : NULL));
+  TINO_FATAL_COND(e->_const, "nonconstant access to constant assoc element");
   return e ? e->v : NULL;
 }
 
@@ -263,23 +314,109 @@ tino_assoc_del(TINO_ASSOC a, const void *k)
       cDP(("() -1 (no)"));
       return -1;
     }
-  _tino_assoc_remove(a, e);
+  _tino_assoc_free(a, e);
   cDP(("() 0 (ok)"));
   return 0;
+}
+
+/* Access to key, value and nonconstant value	*/
+static const void *tino_assoc_key(TINO_ASSOC_ITER i) { return i->k; }
+static const void *tino_assoc_val(TINO_ASSOC_ITER i) { return i->v; }
+static       void *tino_assoc_nval(TINO_ASSOC_ITER i) { TINO_FATAL_COND(i->c, "nonconstant access to constant assoc element"); return i->v; }
+
+/* Important for the movement routines:
+ *
+ * You must call tino_assoc_more() afterwards to make the movement to your current position!
+ */
+
+/* Move to the first element.  Returns:
+ * true:	there is a first element
+ * false:	empty assoc, tino_assoc_more() will free the iterator
+ */
+TINO_INLINE(static int
+tino_assoc_first(TINO_ASSOC_ITER i))
+{
+  i->p	= i->r ? i->a->last : i->a->first;
+  return i->p != 0;
+}
+
+/* Move to the last element.  Returns:
+ * true:	there is a last element
+ * false:	empty assoc, tino_assoc_more() will free the iterator
+ */
+TINO_INLINE(static int
+tino_assoc_last(TINO_ASSOC_ITER i))
+{
+  i->p	= i->r ? i->a->first : i->a->last;
+  return i->p != 0;
+}
+
+/* Move to the next element.
+ * returns true:	there is more
+ * false:		we are at the end, tino_assoc_more() will free the iterator
+ */
+TINO_INLINE(static int
+tino_assoc_next(TINO_ASSOC_ITER i))
+{
+  cDP(("(%a) %p", i, i->p));
+  if (UNLIKELY(!i->p))
+    return 0;
+  i->p	= i->r ? i->p->prev : i->p->next;
+  return 1;
+}
+
+/* Move to the previous element (opposite direction).
+ * returns true:	moved to previous element
+ * false:		already at the first element, unknown if tino_assoc_more() will free the iterator
+ *
+ * If this returns false, you are either on the first element or have an empty list.
+ * In this case, use tino_assoc_first() to see if tino_assoc_more() will free the iterator.
+ *
+ * if (tino_assoc_prev(i))
+ *   {
+ *     tino_assoc_more(i);
+ *     // work with element
+ *   }
+ * else if (!tino_assoc_first(i))
+ *   {
+ *     break;	// empty list, avoid i to be freed by not calling tino_assoc_more()
+ *   }
+ * else
+ *   {
+ *     tino_assoc_more(i);
+ *     // we are now working with the first element
+ *   }
+ */
+TINO_INLINE(static int
+tino_assoc_prev(TINO_ASSOC_ITER i))
+{
+  struct tino_assoc_el	*p;
+
+  cDP(("(%a) %p", i, i->p));
+  if (UNLIKELY(!i->p))
+    p	= i->r ? i->a->last : i->a->first;
+  else
+    p	= i->r ? i->p->next : i->p->prev;
+  if (!p)
+    return 0;
+  i->p	= p;
+  return 1;
 }
 
 /*
  * for (i=tino_assoc_iter(a, 0); tino_assoc_more(i); tino_assoc_next(i))
  *   {
- *     // deletion of the current key is safe, as long you do not access the element afterwards
- *     tino_assoc_key(i);
- *     tino_assoc_val(i);
+ *     // deletion of the current key is safe, as long you do not change the future elements
+ *     tino_assoc_key(i);	// access key
+ *     tino_assoc_val(i);	// access value
+ *     tino_assoc_deli(i);	// delete the current entry
  *   }
- * // iter is automatically freed
+ * // iter is automatically freed by tino_assoc_more
  * or:
  * i	= tino_assoc_iter(a, 0);
  * while (tino_assoc_step(i))
  *   {
+ *     // see above
  *   }
  * // iter is automatically freed
  */
@@ -291,24 +428,35 @@ tino_assoc_iter(TINO_ASSOC a, int rev)
   cDP(("(%a, %d)", a, rev));
   i	= tino_allocO(sizeof *i);
   i->a	= a;
-  i->p	= rev ? a->last : a->first;
-  i->k	= NULL;
-  i->v	= NULL;
-  i->r	= rev;
+  i->e	= 0;
+  i->k	= 0;
+  i->v	= 0;
+  i->c	= 0;
+  i->r	= !!rev;
+  tino_assoc_first(i);
   cDP(("() %p", i));
   return i;
 }
-
-static const void *tino_assoc_key(TINO_ASSOC_ITER i) { return i->k; }
-static const void *tino_assoc_val(TINO_ASSOC_ITER i) { return i->v; }
 
 static void
 tino_assoc_end(TINO_ASSOC_ITER i)
 {
   cDP(("(%a)", i));
+  i->a	= 0;
+  i->e	= 0;
+  i->p	= 0;
+  i->k	= 0;
+  i->v	= 0;
   tino_freeO(i);
 }
 
+/* Do the movement and find out if there is a current element
+ * true:	there is more
+ * false:	at the end, free the iterator
+ *
+ * If you do not want to free the iterator, do not call this routine.
+ * You can use the return values of the movement routines as indicator.
+ */
 TINO_INLINE(static int
 tino_assoc_more(TINO_ASSOC_ITER i))
 {
@@ -318,21 +466,17 @@ tino_assoc_more(TINO_ASSOC_ITER i))
       tino_assoc_end(i);
       return 0;
     }
+  i->e	= i->p;
+  i->c	= i->p->_const;
   i->k	= i->p->k;
   i->v	= i->p->v;
   return 1;
 }
 
-TINO_INLINE(static int
-tino_assoc_next(TINO_ASSOC_ITER i))
-{
-  cDP(("(%a) %p", i, i->p));
-  if (UNLIKELY(!i->p))
-    return 0;
-  i->p	= i->r ? i->p->prev : i->p->next;
-  return 1;
-}
-
+/* Convenience: tino_assoc_next and tino_assoc_more combined
+ * returns true:	there is on element
+ * false:		we are at the end, iterator is freed
+ */
 TINO_INLINE(static int
 tino_assoc_step(TINO_ASSOC_ITER i))
 {
@@ -342,5 +486,41 @@ tino_assoc_step(TINO_ASSOC_ITER i))
   return 1;
 }
 
+/* Convenience: remove the value at current iterator position
+ *
+ * No bad siedeffect if called twice or more often.
+ */
+TINO_INLINE(void
+tino_assoc_deli(TINO_ASSOC_ITER i))
+{
+  if (UNLIKELY(!i->e))
+    return;
+  i->k	= 0;
+  i->v	= 0;
+  _tino_assoc_free(i->a, i->e);
+  i->e	= 0;
+}
+
+/* Convenience: Change current iterator position to constant value
+ */
+TINO_INLINE(void
+tino_assoc_seti(TINO_ASSOC_ITER i, const void *v))
+{
+  if (UNLIKELY(!i->e))
+    return;
+  _tino_assoc_vset(i->a, i->e, (void *)v, 1);
+}
+
+/* Convenience: Change current iterator position to nonconstant value
+ */
+TINO_INLINE(void
+tino_assoc_nseti(TINO_ASSOC_ITER i, void *v))
+{
+  if (UNLIKELY(!i->e))
+    return;
+  _tino_assoc_vset(i->a, i->e, v, 0);
+}
+
 #undef	cDP
 #endif
+
