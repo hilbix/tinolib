00f9e243492a6baa8ce38dc8b9309298ab3f40ec
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-04-11 23:23:25 +0200

    bin/gen.sh macros MAP LOOP GLOB
    ALLINCLUDES retired (can be expressed with
    MAP GLOB and LOOP)

--- bin/gen.sh
+++ bin/gen.sh
@@ -6,7 +6,7 @@
 CHECKSUM_FILE=.generated.checksum
 
 STDOUT() { local e=$?; printf '%q' "$1"; [ 1 -lt $# ] && printf ' %q' "${@:2}"; printf '\n'; return $e; }
-STDERR() { local e=$?; STDOUT "$@" >&2; return e; }
+STDERR() { local e=$?; STDOUT "$@" >&2; return $e; }
 OOPS() { STDERR "OOPS:" "$@"; exit 23; }
 x() { "$@"; }
 o() { x "$@" || OOPS fail $?: "$@"; }
@@ -53,18 +53,23 @@ mvaway()
   o mv -n "$1" "$1.~$max~"
 }
 
-replace()
+: replace-line file prefix cmdargs.. suffix
+replace-line()
 {
-  local cmd="${1%% *}"
-  local arg="${1#"$cmd"}"
-  local ARGS FILE="$2"
-  read -ra ARGS <<<"${arg# }"
-  declare -F -- "do-$cmd" >/dev/null ||
-	{
-	printf '#error unknown generator "%q"\n' "$cmd"
-	return 1
-	}
-  o "do-$cmd" "${ARGS[@]}"
+  local cmd="${3%% *}" args
+  local ARGS="${3#"$cmd"}"
+  local FILE="$1"
+  ARGS="${ARGS# }"
+  read -ra args <<<"$ARGS"
+
+  line=""
+  if	declare -F -- "do-$cmd" >/dev/null
+  then
+	o "do-$cmd" "${args[@]}"
+  else
+	o put '' "$LINENO" '#error removed unknown sequence: "{{%q}}"' "$3"
+  fi
+  line="$2$line$4"
 }
 
 realpath()
@@ -72,50 +77,155 @@ realpath()
   "$(dirname -- "$0")/realpath.sh" "$1"
 }
 
+getline()
+{
+  local a b c
+
+  let lineno++
+  IFS='' read -r line || return
+  [ ".$1" = ".$line" ]  && return
+
+  # Sadly regex do not work here.
+  # Regex:	^(.*)aa(.*?)kk(.*)$
+  # String:	XaabcaabkkkkY
+  # We either see 'X' 'bcaab' 'kk' or 'Xaabc' 'bkk' 'Y'
+  # but both are plain wrong, as we want to see just 'Xaabc' 'b' 'kkY' to process 'b'.
+  while	a="${line##*'{{'}"
+	b="${a%%'}}'*}"
+	[ ".$line" != ".$b" ]
+  do
+	if	[ ".$line" = ".$a" ] || [ ".$a" = ".$b" ]
+	then
+		put '' "$LINENO" '#error unmatched "{{" and "}}"'
+		break
+	fi
+
+	replace-line "$1" "${line%"{{$a"}" "$b" "${a#"$b}}"}"
+  done
+  :
+}
+
+getlines()
+{
+  lines=()
+  while	getline "{{$1}}" || OOPS not found: "{{$1}}" "${@:2}"
+	[ "{{$1}}" != "$line" ]
+  do
+	lines+=("$line")
+  done
+  line=""	# remove {{$1}}
+}
+
 template()
 {
   local name="${1//[^A-Z0-9a-z_]/_}"
   local src="$(realpath "$1")"
 
-  o printf '/* DO NOT EDIT, generated from %q\n' "$1"
-  o printf ' *\n'
-  o printf ' * This Works is placed under the terms of the Copyright Less License,\n'
-  o printf ' * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.\n'
-  o printf ' */\n'
-  o printf '\n'
-  o printf '#ifndef __INC_%s__\n' "$name"
-  o printf '#define __INC_%s__\n' "$name"
-  o printf '\n'
-  let nr=0
-  ok=false
-  while IFS='' read -r line
+  put '' "$LINENO" '/* DO NOT EDIT, generated from %q' "$1"
+  put '' "$LINENO" ' *'
+  put '' "$LINENO" ' * This Works is placed under the terms of the Copyright Less License,'
+  put '' "$LINENO" ' * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.'
+  put '' "$LINENO" ' */'
+  put '' "$LINENO" ''
+  put '' "$LINENO" '#ifndef __INC_%s__' "$name"
+  put '' "$LINENO" '#define __INC_%s__' "$name"
+  put '' "$LINENO" ''
+  lineno=0
+  while	getline
   do
-	let nr++
-	if	[[ "$line" =~ ^(.*)\{\{([^}]*)\}\}(.*)$ ]]
-	then
-		o printf '# %d "%q"\n' "$LINENO" "$ME"
-#		o printf '#pragma MATCH %q\n' "${BASH_REMATCH[@]}"
-		line=""
-		replace "${BASH_REMATCH[2]}" "$1"
-		ok=false
-		line="${BASH_REMATCH[1]}$line${BASH_REMATCH[3]}"
-	fi
-
-	$ok || o printf '# %d "%q"\n' "$nr" "$src"
-	o echo "$line"
-	ok=:
+	put "$src" '' '%s' "$line"
   done
-  o printf '#endif\n'
+  o put '' "$LINENO" '#endif'
 }
+
+putlast=
+putfile=
+
+: put file line data
+put()
+{
+  local file="${1:-$ME}"
+  local nr="${2:-$lineno}"
+  local out tmp
+
+  if	[ ":$putfile:$putlast" != ":$file:$nr" ]
+  then
+	putfile="$file"
+	putlast="$nr"
+	o printf '# %d "%q"\n' "$nr" "$file"
+  fi
+  o printf -vout -- "${@:3}"
+  while	let putlast++
+	tmp="${out%%$'\n'*}"
+	o printf '%s\n' "$tmp"
+	out="${out#"$tmp"}"
+	[ -n "$out" ]
+  do
+	out="${out#$'\n'}"
+  done
+}
+
 
 #### MACROS ####
 
-do-ALLINCLUDES()
+do-LF()
 {
-  for a in lib/*.h
+  line=$'\n'
+}
+
+declare -A maps seps
+
+# {{MAP map sep}}
+# mapdata
+# {{END}}
+# sep is the column separator, default: TAB
+do-MAP()
+{
+  local lines map=()
+
+  eval "${maps["$1"]}";			# does nothing if empty
+  getlines END for MAP started at line "$lineno"
+  map+=("${lines[@]}")
+  maps["$1"]="$(declare -p map)"	# do you have any better idea?
+  seps["$1"]="$2"
+}
+
+do-GLOB()
+{
+  local a b
+
+  for a
   do
-  	[ ".$FILE" = ".$a" ]  || printf '#include "%q"\n' "${a#*/}"
+	for b in $a
+	do
+		[ -e "$b" ] && lines+=("$b")
+	done
   done
+  printf -vline '%s\n' "${lines[@]}"
+}
+
+# {{LOOP map string}}
+# #0# is the map line
+# #1# is the first column
+# #2# is the 2nd column and so on
+do-LOOP()
+{
+  local lines map=() a parm s
+  eval "${maps["$1"]}"			# does nothing if empty
+
+  ARGS="${ARGS#"$1 "}"
+  for a in "${map[@]}"
+  do
+	IFS="${seps["$1"]:-$'\t'}" read -ra parm <<<"$a"
+	parm=("$a" "${parm[@]}")
+	s="$ARGS"
+	while	[[ "$s" =~ ^(.*)#([0-9]*)#(.*)$ ]]
+	do
+		s="${BASH_REMATCH[1]}${parm[${BASH_REMATCH[2]}]}${BASH_REMATCH[3]}"
+	done
+	lines+=("$s")
+  done
+  line="${lines[*]}"
 }
 
 
