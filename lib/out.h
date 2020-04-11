2d4c404c196352a71ff96b9aea92dd7c5d6fd763
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-04-12 00:44:38 +0200

    lib/out.h modified

--- lib/out.h
+++ lib/out.h
@@ -9,13 +9,15 @@
 
 #include "va.h"
 
-typedef void (*outCB)(void *user, const void *, size_t);
+#define	OUTput(FD, ...)	OUTput_(outWriter, (void *)(FD), ##__VA_ARGS__, NULL)
 
 #define	OUTlf	"\n"
 #define	OUT(X)		OUT_TYPE(X), X
 #define	OUTbase(X)	(void *)OUT_BASE, (int)(X)
 #define	OUTw(X)		(void *)OUT_WIDTH, (int)(X)
 #define	OUTfill(X)	(void *)OUT_FILL, (char)(X)
+
+typedef int (*outCB)(void *user, const void *, size_t);	/* return 0:ok else:ERR	*/
 
 {{MAP types}}
 ULL	unsigned long long
@@ -48,29 +50,110 @@ enum
 
 static int ioWrite(int, const void *, int);	/* avoid recursion	*/
 
-static void
+static int
 outWriter(void *user, const void *ptr, size_t len)
 {
   int fd = (int)(uintptr_t)user;
-  int ret;
 
-  ret	= ioWrite(fd, ptr, len);
-  FATAL(ret, "cannot write to fd ", OUT(fd));
+  return ioWrite(fd, ptr, len);
 }
 
-#define	OUTput(FD, ...)	OUTput_(outWriter, (void *)(FD), ##__VA_ARGS__, NULL)
+struct outSet
+  {
+    int		base;	/* default: 10	*/
+    int		width;	/* default: 0	*/
+    int		fill;	/* default: ' '	*/
+    int		sign;	/* default: 0	*/
 
-static void
+    const char	*s;
+    VA_LIST	list;
+
+    outCB	cb;
+    void	*user;
+  };
+
+static int
+OUTput_c(struct outSet *c, const char *s, VA_LIST list)
+{
+  return 0;
+}
+
+static int
 OUTput_(outCB cb, void *user, const char *s, ...)
 {
+  struct outSet	c = { 0 };
+  int		ret;
   VA_LIST	list;
 
+  c.user= user;
+  c.cb	= cb;
   VA_START(list, s);
+
+  ret	= OUTput_c(&c, s, list);
+
+  VA_END(c.list);
+  return ret;
+}
+
+#if 0
+  for (;; s=va_arg(list, void *))
+    {
+      int			type;
+      long long			ll  = 0;	/* shutup compiler	*/
+      unsigned long long	ull = 0;	/* shutup compiler	*/
+      size_t			n;
+
+      switch ((uintptr_t)s)
+        {
+	char c;
+
+        case FORMAT_ARGS:	s	= va_arg(list, const char *); vFORMAT(cb, user, s, *va_arg(list, va_list *)); continue;
+        /* THIS ^^^^^ is missing in printf(), so I need to re-invent the wheel, sigh.	*/
+        case FORMAT_NULL:	return;
+        default:
+          n	= strlen(s);
+          FORMATfill(fill, width-n, cb, user);
+          cb(user, s, n);
+          goto out;
+
+        case FORMAT_BASE:	base	= va_arg(list, int);		continue;
+        case FORMAT_WIDTH:	width	= va_arg(list, int);		continue;
+        case FORMAT_FILL:	fill	= (char)va_arg(list, int);	continue;
+        case FORMAT_SIGN:	sign	= fill;				continue;
+        case FORMAT_PLUS:	sign	= '+';				continue;
+
+        case FORMAT_CHAR:	c	= va_arg(list, int); cb(user, &c, 1); continue;
+
+        case FORMAT_I8:		ll	= (int8_t)va_arg(list, int);	type=1; break;
+        case FORMAT_I16:	ll	= (int16_t)va_arg(list, int);	type=1; break;
+        case FORMAT_I32:	ll	= va_arg(list, int32_t);	type=1; break;
+        case FORMAT_I64:	ll	= va_arg(list, int64_t);	type=1; break;
+        case FORMAT_INT:	ll	= va_arg(list, int);		type=1; break;
+        case FORMAT_LONG:	ll	= va_arg(list, long);		type=1; break;
+        case FORMAT_LL:		ll	= va_arg(list, long);		type=1; break;
+
+        case FORMAT_U8:		ull	= (uint8_t)va_arg(list, int);	type=2; break;
+        case FORMAT_U16:	ull	= (uint16_t)va_arg(list, int);	type=2; break;
+        case FORMAT_U32:	ull	= va_arg(list, uint32_t);	type=2; break;
+        case FORMAT_U64:	ull	= va_arg(list, uint64_t);	type=2; break;
+        case FORMAT_UCHAR:	ull	= (unsigned char)va_arg(list, int);		type=2; break;
+        case FORMAT_UINT:	ull	= va_arg(list, unsigned int);	type=2; break;
+        case FORMAT_ULONG:	ull	= va_arg(list, unsigned long);	type=2; break;
+        case FORMAT_ULL:	ull	= va_arg(list, unsigned long);	type=2; break;
+        }
+      n	= FORMATsigned(type, ull, ll, base, width, fill, sign, cb, user);
+
+out:
+      /* fill it on the right	*/
+      FORMATfill(fill, -width-n, cb, user);
+
+      /* reset format after output	*/
+      base=10, width=0, fill=' ', sign=0;
+    }
   000;
   VA_END(list);
 }
 
-#if 0
 static size_t
 FORMATnr(char *buf, size_t len, unsigned long long n, int base)
 {
@@ -177,62 +260,6 @@ FORMATsigned(int type, unsigned long long ull, long long ll, int base, int width
 static void
 vFORMAT(void (*cb)(void *user, const void *, size_t len), void *user, const char *s, va_list list)
 {
-  int	base=10, width=0, fill=' ', sign=0;
-
-  for (;; s=va_arg(list, void *))
-    {
-      int			type;
-      long long			ll  = 0;	/* shutup compiler	*/
-      unsigned long long	ull = 0;	/* shutup compiler	*/
-      size_t			n;
-
-      switch ((uintptr_t)s)
-        {
-	char c;
-
-        case FORMAT_ARGS:	s	= va_arg(list, const char *); vFORMAT(cb, user, s, *va_arg(list, va_list *)); continue;
-        /* THIS ^^^^^ is missing in printf(), so I need to re-invent the wheel, sigh.	*/
-        case FORMAT_NULL:	return;
-        default:
-          n	= strlen(s);
-          FORMATfill(fill, width-n, cb, user);
-          cb(user, s, n);
-          goto out;
-
-        case FORMAT_BASE:	base	= va_arg(list, int);		continue;
-        case FORMAT_WIDTH:	width	= va_arg(list, int);		continue;
-        case FORMAT_FILL:	fill	= (char)va_arg(list, int);	continue;
-        case FORMAT_SIGN:	sign	= fill;				continue;
-        case FORMAT_PLUS:	sign	= '+';				continue;
-
-        case FORMAT_CHAR:	c	= va_arg(list, int); cb(user, &c, 1); continue;
-
-        case FORMAT_I8:		ll	= (int8_t)va_arg(list, int);	type=1; break;
-        case FORMAT_I16:	ll	= (int16_t)va_arg(list, int);	type=1; break;
-        case FORMAT_I32:	ll	= va_arg(list, int32_t);	type=1; break;
-        case FORMAT_I64:	ll	= va_arg(list, int64_t);	type=1; break;
-        case FORMAT_INT:	ll	= va_arg(list, int);		type=1; break;
-        case FORMAT_LONG:	ll	= va_arg(list, long);		type=1; break;
-        case FORMAT_LL:		ll	= va_arg(list, long);		type=1; break;
-
-        case FORMAT_U8:		ull	= (uint8_t)va_arg(list, int);	type=2; break;
-        case FORMAT_U16:	ull	= (uint16_t)va_arg(list, int);	type=2; break;
-        case FORMAT_U32:	ull	= va_arg(list, uint32_t);	type=2; break;
-        case FORMAT_U64:	ull	= va_arg(list, uint64_t);	type=2; break;
-        case FORMAT_UCHAR:	ull	= (unsigned char)va_arg(list, int);		type=2; break;
-        case FORMAT_UINT:	ull	= va_arg(list, unsigned int);	type=2; break;
-        case FORMAT_ULONG:	ull	= va_arg(list, unsigned long);	type=2; break;
-        case FORMAT_ULL:	ull	= va_arg(list, unsigned long);	type=2; break;
-        }
-      n	= FORMATsigned(type, ull, ll, base, width, fill, sign, cb, user);
-
-out:
-      /* fill it on the right	*/
-      FORMATfill(fill, -width-n, cb, user);
-
-      /* reset format after output	*/
-      base=10, width=0, fill=' ', sign=0;
-    }
 }
 
 #define	FORMAT(X...)	_FORMAT(X, NULL)
