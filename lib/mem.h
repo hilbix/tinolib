55b5e6ffc15298abb2d1d77462aabe89185979c2
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-04-11 02:38:17 +0200

    lib/mem.h added

---
+++ lib/mem.h
@@ -0,0 +1,35 @@
+/* MiniLib
+ *
+ * Memory management
+ */
+
+#include <stdlib.h>
+
+#include "err.h"
+#include "out.h"
+
+static void *
+re_alloc(void *buf, size_t len)
+{
+  buf	= realloc(buf, len);
+  if (!buf)
+    OOPS("out of memory allocating ", OUT(len), " bytes");
+  return buf;
+}
+
+static void *
+alloc(size_t len)
+{
+  return re_alloc(NULL, len);
+}
+
+static void *
+alloc0(size_t len)
+{
+  void *ptr;
+
+  ptr	= alloc(len);
+  memset(ptr, 0, len);
+  return ptr;
+}
+
